b9f1683fadaa48a4ffea0f0f442976efdf638bcc
Ahmed Gamal <agamal0077@gmail.com>
2023-08-14 23:42:57 +0300

    new component + install tachyons framework

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Hello from './Hello'
+import 'tachyons'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello World</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<Hello />, document.getElementById('root'))
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />,<h1>Hello World</h1>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
