bef1d7b5d99858ce2f71778948af2c051178ccbd
stackj007 <agamal0077@gmail.com>
2023-08-15 15:38:54 +0300

    making card component dynamic

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({ name, id, email }) => {
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow">
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
       <img
-        src="https://robohash.org/test?size=200x200"
+        src={`https://robohash.org/${id}?size=200x200`}
         alt="robots"
       />
       <div>
-        <h2>Jane Doe</h2>
-        <p>Jane.doe@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   )
