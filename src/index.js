1f7e322f5bfa909d9b76e0f45a6770e1dca0eb28
stackj007 <agamal0077@gmail.com>
2023-08-15 14:48:48 +0300

    unnecessary files deleted

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import './index.css'
 import Hello from './Hello'
 import 'tachyons'
 
-ReactDOM.render(<Hello />, document.getElementById('root'))
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />,<h1>Hello World</h1>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// )
+ReactDOM.render(
+  <Hello greeting={'Hello ' + 'React Ninja'} />,
+  document.getElementById('root')
+)
