6a1ce7b9073c4cc7e2f9fee6474bd517b626d6da
Ahmed Gamal <agamal0077@gmail.com>
2023-08-17 12:50:11 +0300

    card component that loops over robots

--- src/CardList.js
+++ src/CardList.js
@@ -2,27 +2,17 @@ import React from 'react'
 import Card from './Card'
 
 const CardList = ({ robots }) => {
-  return (
-    <div>
+  const cardComponent = robots.map((user, i) => {
+    return (
       <Card
-        id={robots[0].id}
-        name={robots[0].name}
-        email={robots[0].email}
+        id={robots[i].id}
+        name={robots[i].name}
+        email={robots[i].email}
       />
+    )
+  })
 
-      <Card
-        id={robots[1].id}
-        name={robots[1].name}
-        email={robots[1].email}
-      />
-
-      <Card
-        id={robots[2].id}
-        name={robots[2].name}
-        email={robots[2].email}
-      />
-    </div>
-  )
+  return <div>{cardComponent}</div>
 }
 
 export default CardList
